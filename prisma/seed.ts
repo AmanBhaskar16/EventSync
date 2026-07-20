406c8362744de589345a3275dbc6dac9ab14705e
AmanBhaskar16 <amanbhaskar16@gmail.com>
2026-07-20 08:26:32 +0530

    fix: exclude seed from ts check

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,10 @@
 
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { prisma } from "../src/lib/db/prisma";
 import bcrypt from "bcryptjs";
 import "dotenv/config";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
-const prisma = new PrismaClient({ adapter });
+// const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+// const prisma = new PrismaClient({ adapter });
 
 async function main() {
   const hash = await bcrypt.hash("Demo@1234", 12);
